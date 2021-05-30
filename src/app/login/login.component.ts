3922cfa0e5b5a1f79a351ab78390be4000e2fa2d
Nikhil Dhawan <nikhild64@gmail.com>
2021-05-30 12:15:48 +0530

    priamry color variable

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,9 +9,6 @@ export class LoginComponent implements OnInit {
 
   @Input() websiteName:string|undefined;
   activity='Login'
-
-  //  Websites = 'www.google.com' | 'reddit.com';
-  //  mySite: Websites = 'www.google.com' //pass
   constructor() { }
 
   // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
