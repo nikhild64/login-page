b8dda952183639e678e5308670d1d602d5579968
Nikhil Dhawan <nikhild64@gmail.com>
2021-05-30 18:22:24 +0530

    changed the folder structure for module

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginComponent } from '../login.component';
-import { InputWithLabelComponent } from '../input-with-label/input-with-label.component';
+import { LoginComponent } from './login.component';
+import { InputWithLabelComponent } from './input-with-label/input-with-label.component';
 
 
 
