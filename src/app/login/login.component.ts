36b4fd3ef102367daff7017b08a5712c0dcc5f91
Nikhil Dhawan <nikhild64@gmail.com>
2021-05-30 12:11:42 +0530

    option to add signup

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,11 +8,18 @@ import { Component, Input, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   @Input() websiteName:string|undefined;
+  activity='Login'
+
+  //  Websites = 'www.google.com' | 'reddit.com';
+  //  mySite: Websites = 'www.google.com' //pass
   constructor() { }
 
   // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
   ngOnInit(): void {
 
+  }
+  switchMode(){
+    this.activity=this.activity==='Login'?'Signup':'Login';
   }
 
 
